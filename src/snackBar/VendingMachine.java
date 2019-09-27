8f9679e0b00755fd463e1622cb4e85b57da5d95c
zacharyarney <zachary.arney@gmail.com>
2019-09-26 17:20:34 -0700

    adds VendingMachine methods

--- src/snackBar/VendingMachine.java
+++ src/snackBar/VendingMachine.java
@@ -14,4 +14,24 @@ public class VendingMachine
         id = maxId;
         this.name = name;
     }
+
+    public int getId()
+    {
+        return id;
+    }
+
+    public void setId(int id)
+    {
+        this.id = id;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
 }
