8284c287fff246c6ff296d428f924d0ebff48a8a
zacharyarney <zachary.arney@gmail.com>
2019-09-26 17:12:19 -0700

    Adds Main.java

---
+++ src/snackBar/Main.java
@@ -0,0 +1,13 @@
+package snackBar;
+
+public class Main
+{
+    private static void snackBarProcess()
+    {
+        System.out.println("hgfvhgf");
+    }
+    public static void main (String[] args)
+    {
+        snackBarProcess();
+    }
+}
