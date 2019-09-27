8c8eb1decea0e46ab96f70710e377a44035ca041
zacharyarney <zachary.arney@gmail.com>
2019-09-26 17:16:59 -0700

    Adds Snack methods

--- src/snackBar/Snack.java
+++ src/snackBar/Snack.java
@@ -21,4 +21,71 @@ public class Snack
         this.cost = cost;
         this.vendId = vendId;
     }
+
+    public int getId()
+    {
+        return id;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public int getQuantity()
+    {
+        return quantity;
+    }
+
+    public double getCost()
+    {
+        return cost;
+    }
+
+    public int getVendId()
+    {
+        return vendId;
+    }
+
+    public void setId(int id)
+    {
+        this.id = id;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    public void setCost(double cost)
+    {
+        this.cost = cost;
+    }
+
+    public void setVendId(int vendId)
+    {
+        this.vendId = vendId;
+    }
+
+    public void addQuantity(int amt)
+    {
+        quantity += amt;
+        System.out.printf("%d %s(s) were added.", amt, name);
+    }
+
+    public double getTotalCost(int amt)
+    {
+        return cost * amt;
+    }
+
+    public void buySnack(int amt, String customerName)
+    {
+        if (amt > quantity) {
+            System.out.println("Not enough snacks to buy that many!");
+        } else {
+            double totalCost = getTotalCost(amt);
+            quantity -= amt;
+            System.out.printf("%s bought %d %s(s) for $%.2f.\n", customerName, amt, name, totalCost);
+        }
+    }
 }
